4eed9b337b7cceb61008d596ce1109cbdc1c7400
Gail Hedani <gailhedani@Gails-MacBook-Pro-2.local>
2017-08-05 15:19:34 -1000

    Setup app for heroku - another user

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require("mongoose");
 
 // tell mongoose which promise library to use
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://gail:MyMongo@ds121622.mlab.com:21622/koko")
+mongoose.connect("mongodb://gkhedani:MyMongo@ds121622.mlab.com:21622/koko")
 // local:  "mongodb://localhost:27017/TodoApp");
 
 module.exports = {
