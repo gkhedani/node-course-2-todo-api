0603505436b7d6a0c82d00f72091ee024c996777
Gail Hedani <gailhedani@Gails-MacBook-Pro-2.local>
2017-08-06 15:32:40 -1000

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 const {ObjectID} = require("mongodb");
 const port = process.env.PORT || 3000;
-
-var express = require("express");
-var bodyParser = require("body-parser");
+const _ = require("lodash");
+const express = require("express");
+const bodyParser = require("body-parser");
 
 var {mongoose} = require("./db/mongoose");
 var {Todo} = require("./models/todo");
@@ -71,6 +71,30 @@ app.delete("/todos/:id", (req, res) => {
     // success if no doc, 404
     // otherwise doc w/200
     // error - 400 w/empty body
+});
+
+app.patch("/todos/:id", (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ["text", "completed"]);
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => res.status(400).send());
 });
 
 app.listen(port, () => {
